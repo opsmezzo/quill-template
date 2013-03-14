8f27fc5f38d552e2192f00d4ead75c6d531f06fc
indexzero <charlie.robbins@gmail.com>
2013-03-14 08:19:50 -0400

    [test] Added tests for falsy and inverted sections.

--- test/template-test.js
+++ test/template-test.js
@@ -80,7 +80,9 @@ vows.describe('quill-template/template').addBatch({
     ['{{foo}}{{bar}}', 'lvaluervalue'],
     ['{{ foo}}{{bar}}', 'lvaluervalue'],
     ['{{foo }}{{bar}}', 'lvaluervalue'],
-    ['{{foo }}{{ bar}}', 'lvaluervalue']
+    ['{{foo }}{{ bar}}', 'lvaluervalue'],
+    ['{{#missing}}{{.}}{{/missing}} {{foo}}', ' lvalue'],
+    ['{{^missing}}MISSING!{{/missing}} {{foo}}', 'MISSING! lvalue']
   ].reduce(function (tests, args) {
     tests[args[0]] = shouldRender.apply(null, args);
     return tests;
